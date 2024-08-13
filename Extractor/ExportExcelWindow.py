60737eb8b1e30f126f2035feaab50afceba8bd3f
raballito <qraball@gmail.com>
2024-08-13 14:41:20 +0200

    Correction de la fenêtre d'export vers excel

--- Extractor/ExportExcelWindow.py
+++ Extractor/ExportExcelWindow.py
@@ -6,6 +6,7 @@ Excel Export Window
 - Selection of the desired options
 - Availablility depends on the analysis of the sample
 - Export in Output directory
+- Export everything in Excel file
 
 Version: Beta 1.3
 Last Update: 13.08.24
@@ -20,6 +21,7 @@ import openpyxl
 from openpyxl.drawing.image import Image
 import matplotlib.pyplot as plt
 import os
+import shutil
 from tkinter import messagebox
 
 class ExportExcelWindow(customtkinter.CTkToplevel):
@@ -53,17 +55,17 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
         customtkinter.CTkLabel(self, text="Choisissez les options pour l'exportation sous Excel:", font=("Arial", 12, "bold")).pack(pady=10)
         
         # Section des options principales
-        self.check_graphics = customtkinter.CTkCheckBox(self, text="Inclure les graphiques", variable=self.selected_options['graphics'])
-        self.check_analysis = customtkinter.CTkCheckBox(self, text="Inclure l'analyse", variable=self.selected_options['analysis'])
-        self.check_summary = customtkinter.CTkCheckBox(self, text="Inclure le résumé", variable=self.selected_options['summary'])
-        self.check_stress_values = customtkinter.CTkCheckBox(self, text="Inclure les valeurs de contrainte", variable=self.selected_options['stress_values'])
-        self.check_deformation_values = customtkinter.CTkCheckBox(self, text="Inclure les valeurs de déformation", variable=self.selected_options['deformation_values'])
+        self.option_graphics = customtkinter.CTkCheckBox(self, text="Inclure les graphiques", variable=self.selected_options['graphics'])
+        self.option_analysis = customtkinter.CTkCheckBox(self, text="Inclure l'analyse", variable=self.selected_options['analysis'])
+        self.option_summary = customtkinter.CTkCheckBox(self, text="Inclure le résumé", variable=self.selected_options['summary'])
+        self.option_stress_values = customtkinter.CTkCheckBox(self, text="Inclure les valeurs de contrainte", variable=self.selected_options['stress_values'])
+        self.option_deformation_values = customtkinter.CTkCheckBox(self, text="Inclure les valeurs de déformation", variable=self.selected_options['deformation_values'])
 
-        self.check_graphics.pack(anchor='w', padx=20, pady=5)
-        self.check_analysis.pack(anchor='w', padx=20, pady=5)
-        self.check_summary.pack(anchor='w', padx=20, pady=5)
-        self.check_stress_values.pack(anchor='w', padx=20, pady=5)
-        self.check_deformation_values.pack(anchor='w', padx=20, pady=5)
+        self.option_graphics.pack(anchor='w', padx=20, pady=5)
+        self.option_analysis.pack(anchor='w', padx=20, pady=5)
+        self.option_summary.pack(anchor='w', padx=20, pady=5)
+        self.option_stress_values.pack(anchor='w', padx=20, pady=5)
+        self.option_deformation_values.pack(anchor='w', padx=20, pady=5)
 
         # Boutons Exporter et Annuler
         self.button_export = customtkinter.CTkButton(self, text="Exporter", command=lambda: self.get_options()).pack(pady=20)        
@@ -73,49 +75,49 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
 
     def update_checkbox_state(self, sample_list):
         if len(sample_list) == 0:
-            self.check_graphics.deselect()
-            self.check_analysis.deselect()
-            self.check_summary.deselect()
-            self.check_stress_values.deselect()
-            self.check_deformation_values.deselect()
+            self.option_graphics.deselect()
+            self.option_analysis.deselect()
+            self.option_summary.deselect()
+            self.option_stress_values.deselect()
+            self.option_deformation_values.deselect()
             
-            self.check_graphics.configure(state='disabled')
-            self.check_analysis.configure(state='disabled')
-            self.check_summary.configure(state='disabled')
-            self.check_stress_values.configure(state='disabled')
-            self.check_deformation_values.configure(state='disabled')
+            self.option_graphics.configure(state='disabled')
+            self.option_analysis.configure(state='disabled')
+            self.option_summary.configure(state='disabled')
+            self.option_stress_values.configure(state='disabled')
+            self.option_deformation_values.configure(state='disabled')
         else:
             # Vérifier si tous les échantillons sont analysés
             all_analyzed = all(sample.analyzed_sample for sample in self.sample_list)
     
             if not all_analyzed:
-                self.check_analysis.deselect()
-                self.check_summary.deselect()
-                self.check_stress_values.deselect()
-                self.check_deformation_values.deselect()
+                self.option_analysis.deselect()
+                self.option_summary.deselect()
+                self.option_stress_values.deselect()
+                self.option_deformation_values.deselect()
     
-                self.check_analysis.configure(state='disabled')
-                self.check_summary.configure(state='disabled')
-                self.check_stress_values.configure(state='disabled')
-                self.check_deformation_values.configure(state='disabled')
+                self.option_analysis.configure(state='disabled')
+                self.option_summary.configure(state='disabled')
+                self.option_stress_values.configure(state='disabled')
+                self.option_deformation_values.configure(state='disabled')
             else:
-                self.check_analysis.configure(state='normal')
-                self.check_summary.configure(state='normal')
-                self.check_stress_values.configure(state='normal')
-                self.check_deformation_values.configure(state='normal')
+                self.option_analysis.configure(state='normal')
+                self.option_summary.configure(state='normal')
+                self.option_stress_values.configure(state='normal')
+                self.option_deformation_values.configure(state='normal')
     
                 if not self.selected_options['analysis'].get():
-                    self.check_summary.deselect()
-                    self.check_stress_values.deselect()
-                    self.check_deformation_values.deselect()
+                    self.option_summary.deselect()
+                    self.option_stress_values.deselect()
+                    self.option_deformation_values.deselect()
     
-                    self.check_summary.configure(state='disabled')
-                    self.check_stress_values.configure(state='disabled')
-                    self.check_deformation_values.configure(state='disabled')
+                    self.option_summary.configure(state='disabled')
+                    self.option_stress_values.configure(state='disabled')
+                    self.option_deformation_values.configure(state='disabled')
                 else:
-                    self.check_summary.configure(state='normal')
-                    self.check_stress_values.configure(state='normal')
-                    self.check_deformation_values.configure(state='normal')
+                    self.option_summary.configure(state='normal')
+                    self.option_stress_values.configure(state='normal')
+                    self.option_deformation_values.configure(state='normal')
 
     def get_options(self):
         # Collecter les options sélectionnées
@@ -139,6 +141,11 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
         
         # Créer un fichier Excel
         writer = pd.ExcelWriter(file_path, engine='openpyxl')
+        
+        # Créer un dossier temporaire pour les graphiques
+        self.temp_dir = 'temp'
+        if not os.path.exists(self.temp_dir):
+            os.makedirs(self.temp_dir)
     
         # Ajouter le résumé
         if include_summary:
@@ -166,7 +173,7 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
     
         # Ajout des graphiques (si sélectionnés)
         if include_graphics:
-            self.generate_and_add_graphics(summary_sheet)
+            self.generate_and_add_graphics(summary_sheet, sample_list)
     
         # Collecte des données pour le résumé
         summary_data = {
@@ -228,13 +235,34 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
     
     
     def add_sample_graph(self, writer, sample):
-        #Ajoute un graphique des données de l'échantillon à la feuille correspondante.
-        fig, ax = plt.subplots()
-        ax.plot(sample.displacement_values, sample.force_values)  # Graphique exemple
-        plt.savefig('temp_graph.png')
-        img = openpyxl.drawing.image.Image('temp_graph.png')
-        writer.sheets[f'Echantillon {sample.sample_name}'].add_image(img, 'G1')
-    
+        # Graphique force-déplacement
+        fig1, ax1 = plt.subplots()
+        self.plot_force_displacement(ax1, [sample])
+        
+        # Utiliser un nom unique pour chaque fichier d'image
+        graph1_filename = os.path.join(self.temp_dir, f'sample_graph1_{sample.sample_name}.png')
+        plt.savefig(graph1_filename)
+        
+        # Ajouter l'image au fichier Excel
+        img1 = openpyxl.drawing.image.Image(graph1_filename)
+        writer.sheets[f'Echantillon {sample.sample_name}'].add_image(img1, 'G1')
+        
+        # Graphique contrainte-déformation
+        fig2, ax2 = plt.subplots()
+        self.plot_stress_deformation(ax2, [sample])
+        
+        # Utiliser un nom unique pour chaque fichier d'image
+        graph2_filename = os.path.join(self.temp_dir, f'sample_graph2_{sample.sample_name}.png')
+        plt.savefig(graph2_filename)
+        
+        # Ajouter l'image au fichier Excel
+        img2 = openpyxl.drawing.image.Image(graph2_filename)
+        writer.sheets[f'Echantillon {sample.sample_name}'].add_image(img2, 'N1')
+        
+        # Fermer les figures pour libérer de la mémoire
+        plt.close(fig1)
+        plt.close(fig2)
+        
     
     def finalize_export(self, writer, sample_list):
         #Sauvegarde et ferme le fichier Excel.
@@ -244,38 +272,44 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
         print(end_message)
         messagebox.showinfo("Exportation terminée", end_message)
         writer.close()
+        shutil.rmtree(self.temp_dir)
             
-    def generate_and_add_graphics(self, summary_sheet):
+    def generate_and_add_graphics(self, summary_sheet, sample_list):
         # Graphique 1: Contrainte-Déformation
         fig1, ax1 = plt.subplots()
-        self.plot_stress_deformation(ax1)
-        plt.savefig('graph1.png')
-        img1 = Image('graph1.png')
+        self.plot_stress_deformation(ax1, sample_list)
+        graph1_filename = os.path.join(self.temp_dir, 'summary_graph1.png')
+        plt.savefig(graph1_filename)
+        img1 = Image(graph1_filename)
         summary_sheet.add_image(img1, 'I1')
 
         # Graphique 2: Force-Déplacement
         fig2, ax2 = plt.subplots()
-        self.plot_force_displacement(ax2)
-        plt.savefig('graph2.png')
-        img2 = Image('graph2.png')
+        self.plot_force_displacement(ax2, sample_list)
+        graph2_filename = os.path.join(self.temp_dir, 'summary_graph2.png')
+        plt.savefig(graph2_filename)
+        img2 = Image(graph2_filename)
         summary_sheet.add_image(img2, 'P1')
 
         # Graphique 3: Contrainte-Déplacement
         fig3, ax3 = plt.subplots()
-        self.plot_stress_displacement(ax3)
-        plt.savefig('graph3.png')
-        img3 = Image('graph3.png')
+        self.plot_stress_displacement(ax3, sample_list)
+        graph3_filename = os.path.join(self.temp_dir, 'summary_graph3.png')
+        plt.savefig(graph3_filename)
+        img3 = Image(graph3_filename)
         summary_sheet.add_image(img3, 'W1')
 
         plt.close(fig1)
         plt.close(fig2)
         plt.close(fig3)
+        
+        
 
-    def plot_stress_deformation(self, ax):
+    def plot_stress_deformation(self, ax, sample_list):
         max_stress = 0
         max_deformation = 0
         
-        for sample in self.sample_list:
+        for sample in sample_list:
             max_stress = max(max_stress, max(sample.stress_values))
             max_deformation = max(max_deformation, max(sample.deformation_values))
             
@@ -293,11 +327,11 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
         if self.option_legend:
             ax.legend()
 
-    def plot_force_displacement(self, ax):
+    def plot_force_displacement(self, ax, sample_list):
         max_force = 0
         max_displacement = 0
 
-        for sample in self.sample_list:
+        for sample in sample_list:
             max_force = max(max_force, max(sample.force_values))
             max_displacement = max(max_displacement, max(sample.displacement_values))
             
@@ -308,15 +342,15 @@ class ExportExcelWindow(customtkinter.CTkToplevel):
 
         ax.set_title('Force-Déplacement')
         ax.set_xlabel('Déplacement [mm]')
-        ax.set_ylabel('Force [N]')
+        ax.set_ylabel('Force [N]') if not self.option_kn else ax.set_xlabel('Force [kN]')
         if self.option_legend:
             ax.legend()
 
-    def plot_stress_displacement(self, ax):
+    def plot_stress_displacement(self, ax, sample_list):
         max_stress = 0
         max_displacement = 0
 
-        for sample in self.sample_list:
+        for sample in sample_list:
             max_stress = max(max_stress, max(sample.stress_values))
             max_displacement = max(max_displacement, max(sample.displacement_values))
             
